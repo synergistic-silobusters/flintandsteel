8070d4ba377e666adbd44f830ee154732e3bb405
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-05-07 23:44:25 -0400

    User is now toasted when logging in or signing up

--- src/login/login.js
+++ src/login/login.js
@@ -4,13 +4,28 @@ angular.module('flintAndSteel')
 .controller('loginCtrl', [
 		'$scope',
 		'$state',
+		'$mdToast',
 		'loginSvc',
-		function($scope, $state, loginSvc) {
+		function($scope, $state, $mdToast, loginSvc) {
 			$scope.loginUser = function(account) {
 				loginSvc.checkLogin(account, function LoginSuccess(data) {
 					if (data === 'AUTH_OK') {
+						$mdToast.show(
+							$mdToast.simple()
+								.content(account.username + ' has successfully signed in!')
+								.position('top right')
+								.hideDelay(5000)
+						)
 						$scope.$root.authenticated = true;
 						$state.go('home');
+					}
+					else if (data === 'AUTH_ERROR') {
+						$mdToast.show(
+							$mdToast.simple()
+								.content('Your credentials don\'t match the stored ones :(')
+								.position('top right')
+								.hideDelay(5000)
+						)
 					}
 					
 				},
