6a11ac17ab777e617b2a0f1684441ca5a1fdb14a
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-05-26 00:51:02 +0530

    rewired serving the mock idea

--- src/ideas/ideaSvc/ideaSvc.js
+++ src/ideas/ideaSvc/ideaSvc.js
@@ -4,53 +4,9 @@ angular.module('flintAndSteel')
 .factory('ideaSvc', 
 	[
 		'$http',
-		function($http) {
+		'ideaSvcMock',
+		function($http, ideaSvcMock) {
 
-			var mockIdea = {
-				id: 'mock_idea',
-				title: 'The bestest Idea ever!',
-				description: 'Apophenia order-flow systema futurity garage sentient car advert. Footage 3D-printed Legba free-market lights courier camera Kowloon youtube fluidity euro-pop garage bicycle augmented reality. Dome military-grade faded meta--space vehicle warehouse. Computer concrete corrupted vehicle tower dead knife cyber-camera augmented reality table shrine apophenia tiger-team-ware soul-delay. Hacker pistol into plastic realism sub-orbital futurity girl geodesic disposable boat sentient tanto urban. Plastic alcohol bicycle carbon courier spook gang wristwatch katana sensory sign long-chain hydrocarbons assault nano. ',
-				author: 'Yash Kulshrestha',
-				image: '../assets/defaultideahero.jpg',
-				likes: 23,
-				managerLikes: 6,
-				comments: [
-					{
-						text: 'This sounds cool',
-						from: 'Nobody',
-						time: moment().subtract(1, 'days').calendar()
-					},
-					{
-						text: 'Hey, I was thinking the same thing!',
-						from: 'Another Nobody',
-						time: moment().subtract(4, 'hours').calendar()
-					},
-					{
-						text: 'This is gold, gold I tell you!',
-						from: 'The Man',
-						time: moment().subtract(30, 'minutes').calendar()
-					}
-				],
-				backs: [
-					{
-						text: 'management experience',
-						from: 'Some Manager',
-						time: moment().subtract(7, 'days').calendar(),
-						types: [
-							{ name: 'Experience' },
-							{ name: 'Knowledge' }
-						]
-					},
-					{
-						text: 'TEN MILLION DOLLARS',
-						from: 'Just Kidding',
-						time: moment().subtract(84, 'hours').calendar(),
-						types: [
-							{ name: 'Funding' }
-						]
-					}
-				]
-			};
 			this.postIdea = function postIdea(idea, successCb, errorCb){
 				$http.get('/uniqueid?for=idea')
 					.success(function getIdSucess(data) {
@@ -66,7 +22,7 @@ angular.module('flintAndSteel')
 			};
 			this.getIdea = function getIdea(ideaId, successCb, errorCb) {
 				if (ideaId === 'mock_idea') {
-					successCb(mockIdea);
+					ideaSvcMock.getIdea(ideaId, successCb);
 				}
 				else {
 					$http.get('/idea?id=' + ideaId)
