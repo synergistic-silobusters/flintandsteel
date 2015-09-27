703fbafe9fbdd4c2a835c6cce8a96d4710a5021e
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-27 17:44:27 -0500

    accountView formatting

--- src/users/accountView/accountView.spec.js
+++ src/users/accountView/accountView.spec.js
@@ -1,49 +1,49 @@
 describe('AccountViewCtrl', function() {
-	var scope, ctrl, $state, $mdToast, loginSvcMock;
+    var scope, ctrl, $state, $mdToast, loginSvcMock;
 
-	beforeEach(module('flintAndSteel'));
-	beforeEach(module('ui.router'));
+    beforeEach(module('flintAndSteel'));
+    beforeEach(module('ui.router'));
 
-	beforeEach(inject(function ($rootScope, $controller, _$state_, _$mdToast_, _loginSvcMock_) {
-		scope = $rootScope.$new();
-		$state = _$state_;
-		$mdToast = _$mdToast_;
-		loginSvcMock = _loginSvcMock_;
+    beforeEach(inject(function ($rootScope, $controller, _$state_, _$mdToast_, _loginSvcMock_) {
+        scope = $rootScope.$new();
+        $state = _$state_;
+        $mdToast = _$mdToast_;
+        loginSvcMock = _loginSvcMock_;
 
-		spyOn($state, 'go');
-		spyOn(loginSvcMock, 'isUserLoggedIn').and.callFake(function() {
-			return false;
-		});
+        spyOn($state, 'go');
+        spyOn(loginSvcMock, 'isUserLoggedIn').and.callFake(function() {
+            return false;
+        });
 
-		ctrl = $controller('AccountViewCtrl', {
-			$scope: scope,
-			$state: $state,
-			$mdToast: $mdToast,
-			loginSvc: loginSvcMock
-		});
-	}));
+        ctrl = $controller('AccountViewCtrl', {
+            $scope: scope,
+            $state: $state,
+            $mdToast: $mdToast,
+            loginSvc: loginSvcMock
+        });
+    }));
 
-	it('should exist', function() {
-		expect(ctrl).toBeDefined();
-	});
+    it('should exist', function() {
+        expect(ctrl).toBeDefined();
+    });
 
-	it('should navigate to home if no user is logged in', function() {
-		expect($state.go).toHaveBeenCalledWith('home');
-	});
+    it('should navigate to home if no user is logged in', function() {
+        expect($state.go).toHaveBeenCalledWith('home');
+    });
 
-	describe('$scope.logout', function() {
-		
-		beforeEach(function() {
-			spyOn(loginSvcMock, 'logout').and.callFake(function() { });
-			spyOn($mdToast, 'show');
-		});
+    describe('$scope.logout', function() {
+        
+        beforeEach(function() {
+            spyOn(loginSvcMock, 'logout').and.callFake(function() { });
+            spyOn($mdToast, 'show');
+        });
 
-		it('should log out the user', function() {
-			scope.logout();
+        it('should log out the user', function() {
+            scope.logout();
 
-			expect(loginSvcMock.logout).toHaveBeenCalled();
-			expect($mdToast.show).toHaveBeenCalled();
-			expect($state.go).toHaveBeenCalledWith('home');
-		});
-	});
+            expect(loginSvcMock.logout).toHaveBeenCalled();
+            expect($mdToast.show).toHaveBeenCalled();
+            expect($state.go).toHaveBeenCalledWith('home');
+        });
+    });
 });
