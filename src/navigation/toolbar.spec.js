4054f3f0af4daa0b4d3042706fec392ec1681d40
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-27 17:42:41 -0500

    toolbar formatting

--- src/navigation/toolbar.spec.js
+++ src/navigation/toolbar.spec.js
@@ -1,55 +1,55 @@
 describe('ToolbarCtrl', function() {
-	var scope, ctrl, $state, $stateParams, $mdSidenav, loginSvcMock;
+    var scope, ctrl, $state, $stateParams, $mdSidenav, loginSvcMock;
 
-	beforeEach(module('flintAndSteel'));
-	beforeEach(module('ui.router'));
+    beforeEach(module('flintAndSteel'));
+    beforeEach(module('ui.router'));
 
-	beforeEach(inject(function ($rootScope, $controller, _$state_, _$stateParams_, _$mdSidenav_, _loginSvcMock_) {
-		scope = $rootScope.$new();
-		$state = _$state_;
-		$stateParams = _$stateParams_;
-		$mdSidenav = _$mdSidenav_;
-		loginSvcMock = _loginSvcMock_;
+    beforeEach(inject(function ($rootScope, $controller, _$state_, _$stateParams_, _$mdSidenav_, _loginSvcMock_) {
+        scope = $rootScope.$new();
+        $state = _$state_;
+        $stateParams = _$stateParams_;
+        $mdSidenav = _$mdSidenav_;
+        loginSvcMock = _loginSvcMock_;
 
-		ctrl = $controller('ToolbarCtrl', {
-			$scope: scope,
-			$state: $state,
-			$stateParams: $stateParams,
-			$mdSidenav: $mdSidenav,
-			loginSvc: loginSvcMock
-		});
-	}));
+        ctrl = $controller('ToolbarCtrl', {
+            $scope: scope,
+            $state: $state,
+            $stateParams: $stateParams,
+            $mdSidenav: $mdSidenav,
+            loginSvc: loginSvcMock
+        });
+    }));
 
-	it('should exist', function() {
-		expect(ctrl).toBeDefined();
-	});
+    it('should exist', function() {
+        expect(ctrl).toBeDefined();
+    });
 
-	describe('$scope.accountClick', function() {
+    describe('$scope.accountClick', function() {
 
-		beforeEach(function() {
-			spyOn($state, 'go');
-		});
+        beforeEach(function() {
+            spyOn($state, 'go');
+        });
 
-		it('should navigate to login if no user is logged in', function() {
-			spyOn(loginSvcMock, 'isUserLoggedIn').and.callFake(function() {
-				return false;
-			});
+        it('should navigate to login if no user is logged in', function() {
+            spyOn(loginSvcMock, 'isUserLoggedIn').and.callFake(function() {
+                return false;
+            });
 
-			scope.accountClick();
+            scope.accountClick();
 
-			expect(loginSvcMock.isUserLoggedIn).toHaveBeenCalled();
-			expect($state.go).toHaveBeenCalledWith('login', {'retState': '', 'retParams': undefined});
-		});
+            expect(loginSvcMock.isUserLoggedIn).toHaveBeenCalled();
+            expect($state.go).toHaveBeenCalledWith('login', {'retState': '', 'retParams': undefined});
+        });
 
-		it('should navigate to account if user is logged in', function() {
-			spyOn(loginSvcMock, 'isUserLoggedIn').and.callFake(function() {
-				return true;
-			});
+        it('should navigate to account if user is logged in', function() {
+            spyOn(loginSvcMock, 'isUserLoggedIn').and.callFake(function() {
+                return true;
+            });
 
-			scope.accountClick();
+            scope.accountClick();
 
-			expect(loginSvcMock.isUserLoggedIn).toHaveBeenCalled();
-			expect($state.go).toHaveBeenCalledWith('account');
-		});
-	});
+            expect(loginSvcMock.isUserLoggedIn).toHaveBeenCalled();
+            expect($state.go).toHaveBeenCalledWith('account');
+        });
+    });
 });
