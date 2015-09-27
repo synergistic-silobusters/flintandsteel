ccf7fb3810a2675211b7c6b8cfcee6c626a094e4
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-27 17:52:24 -0500

    index.html and app.js formatting

--- src/app.js
+++ src/app.js
@@ -2,112 +2,101 @@
 
 
 /*
-	This sets up the main app module and also tells Angular
-	what this module depends on to work correctly.
+    This sets up the main app module and also tells Angular
+    what this module depends on to work correctly.
  */
 angular.module('flintAndSteel', [
-		'ngAnimate',
-		'ui.router',
-		'ngMaterial'
-	]
+        'ngAnimate',
+        'ui.router',
+        'ngMaterial'
+    ]
 )
 .config([
-	'$urlRouterProvider',
-	'$stateProvider',
-	'$mdIconProvider',
-	'$mdThemingProvider',
-	function($urlRouterProvider, $stateProvider, $mdIconProvider, $mdThemingProvider) {
-		$stateProvider
-			.state('home', {
-	      		url: '/',
-	      		templateUrl: 'homeView/homeView.tpl.html',
-	      		controller: 'HomeViewCtrl'
-    		})
-    		.state('login', {
-	      		url: '/login?retState&retParams',
-	      		templateUrl: 'users/loginView/loginView.tpl.html',
-	      		controller: 'LoginViewCtrl'
-    		})
-    		.state('signup', {
-    			url: '/signup',
-    			templateUrl: 'users/signupView/signupView.tpl.html',
-    			controller: 'SignupViewCtrl'
-    		})
-    		.state('account', {
-    			url: '/account',
-    			templateUrl: 'users/accountView/accountView.tpl.html',
-    			controller: 'AccountViewCtrl'
-    		})
-    		.state('ideaslist', {
-    			url: '/ideaslist',
-    			templateUrl: 'ideas/ideasList/ideasList.tpl.html',
-    			controller: 'IdeasListCtrl'
-    		})
-    		.state('idea', {
-    			url: '/idea/{ideaId}',
-    			templateUrl: 'ideas/ideasView/ideasView.tpl.html',
-    			controller: 'IdeasViewCtrl',
-    			controllerAs: 'ideasView'
-    		})
-    		.state('addidea', {
-    			url: '/addidea',
-    			templateUrl: 'ideas/addIdeaView/addIdeaView.tpl.html',
-    			controller: 'AddIdeaViewCtrl'
-    		})
-    		.state('ideabrowse', {
-    			url: '/ideabrowse',
-    			templateUrl: 'ideas/ideaBrowseView/ideaBrowseView.tpl.html',
-    			controller: 'IdeaBrowseViewCtrl'
-    		});
+    '$urlRouterProvider', '$stateProvider', '$mdIconProvider', '$mdThemingProvider',
+    function($urlRouterProvider, $stateProvider, $mdIconProvider, $mdThemingProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'homeView/homeView.tpl.html',
+                controller: 'HomeViewCtrl'
+            })
+            .state('login', {
+                url: '/login?retState&retParams',
+                templateUrl: 'users/loginView/loginView.tpl.html',
+                controller: 'LoginViewCtrl'
+            })
+            .state('signup', {
+                url: '/signup',
+                templateUrl: 'users/signupView/signupView.tpl.html',
+                controller: 'SignupViewCtrl'
+            })
+            .state('account', {
+                url: '/account',
+                templateUrl: 'users/accountView/accountView.tpl.html',
+                controller: 'AccountViewCtrl'
+            })
+            .state('ideaslist', {
+                url: '/ideaslist',
+                templateUrl: 'ideas/ideasList/ideasList.tpl.html',
+                controller: 'IdeasListCtrl'
+            })
+            .state('idea', {
+                url: '/idea/{ideaId}',
+                templateUrl: 'ideas/ideasView/ideasView.tpl.html',
+                controller: 'IdeasViewCtrl',
+                controllerAs: 'ideasView'
+            })
+            .state('addidea', {
+                url: '/addidea',
+                templateUrl: 'ideas/addIdeaView/addIdeaView.tpl.html',
+                controller: 'AddIdeaViewCtrl'
+            })
+            .state('ideabrowse', {
+                url: '/ideabrowse',
+                templateUrl: 'ideas/ideaBrowseView/ideaBrowseView.tpl.html',
+                controller: 'IdeaBrowseViewCtrl'
+            });
 
-		$urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/');
 
-	    $mdIconProvider
-	        .iconSet('action', './assets/icons/action-icons.svg', 24)
-	        .iconSet('alert', './assets/icons/alert-icons.svg', 24)
-	        .iconSet('av', './assets/icons/av-icons.svg', 24)
-	        .iconSet('communication', './assets/icons/communication-icons.svg', 24)
-	        .iconSet('content', './assets/icons/content-icons.svg', 24)
-	        .iconSet('device', './assets/icons/device-icons.svg', 24)
-	        .iconSet('editor', './assets/icons/editor-icons.svg', 24)
-	        .iconSet('file', './assets/icons/file-icons.svg', 24)
-	        .iconSet('hardware', './assets/icons/hardware-icons.svg', 24)
-	        .iconSet('icons', './assets/icons/icons-icons.svg', 24)
-	        .iconSet('image', './assets/icons/image-icons.svg', 24)
-	        .iconSet('maps', './assets/icons/maps-icons.svg', 24)
-	        .iconSet('navigation', './assets/icons/navigation-icons.svg', 24)
-	        .iconSet('notification', './assets/icons/notification-icons.svg', 24)
-	        .iconSet('social', './assets/icons/social-icons.svg', 24)
-	        .iconSet('toggle', './assets/icons/toggle-icons.svg', 24);
+        $mdIconProvider
+            .iconSet('action', './assets/icons/action-icons.svg', 24)
+            .iconSet('alert', './assets/icons/alert-icons.svg', 24)
+            .iconSet('av', './assets/icons/av-icons.svg', 24)
+            .iconSet('communication', './assets/icons/communication-icons.svg', 24)
+            .iconSet('content', './assets/icons/content-icons.svg', 24)
+            .iconSet('device', './assets/icons/device-icons.svg', 24)
+            .iconSet('editor', './assets/icons/editor-icons.svg', 24)
+            .iconSet('file', './assets/icons/file-icons.svg', 24)
+            .iconSet('hardware', './assets/icons/hardware-icons.svg', 24)
+            .iconSet('icons', './assets/icons/icons-icons.svg', 24)
+            .iconSet('image', './assets/icons/image-icons.svg', 24)
+            .iconSet('maps', './assets/icons/maps-icons.svg', 24)
+            .iconSet('navigation', './assets/icons/navigation-icons.svg', 24)
+            .iconSet('notification', './assets/icons/notification-icons.svg', 24)
+            .iconSet('social', './assets/icons/social-icons.svg', 24)
+            .iconSet('toggle', './assets/icons/toggle-icons.svg', 24);
 
-		var rockstarterRedMap = $mdThemingProvider.extendPalette('red', {
-			'900': '650100'
-	    });
-	    var rockstarterGrayMap = $mdThemingProvider.extendPalette('grey', {
-			'900': '464b51'
-	    });
+        var rockstarterRedMap = $mdThemingProvider.extendPalette('red', {
+            '900': '650100'
+        });
+        var rockstarterGrayMap = $mdThemingProvider.extendPalette('grey', {
+            '900': '464b51'
+        });
 
-	    $mdThemingProvider.definePalette('rokstarter-red', rockstarterRedMap);
-	    $mdThemingProvider.definePalette('rokstarter-gray', rockstarterGrayMap);
+        $mdThemingProvider.definePalette('rokstarter-red', rockstarterRedMap);
+        $mdThemingProvider.definePalette('rokstarter-gray', rockstarterGrayMap);
 
-	    $mdThemingProvider.theme('default')
-                          .primaryPalette('rokstarter-gray', {
-                          	'default': '900'
-                          })
-                          .accentPalette('rokstarter-red', {
-                          	'default': '900',
-                          	'hue-1': 'A700'
-                          })
-                          .warnPalette('red');
-
-	    /*$mdThemingProvider.theme('default')
-                          .primaryPalette('gray')
-                          .accentPalette('green', {
-                          	'default': 'A200',
-                          	'hue-1': '600'
-                          })
-                          .warnPalette('red');*/
-	}
+        $mdThemingProvider.theme('default')
+        .primaryPalette('rokstarter-gray', {
+            'default': '900'
+        })
+        .accentPalette('rokstarter-red', {
+            'default': '900',
+            'hue-1': 'A700'
+        })
+        .warnPalette('red');
+    }
 ]);
 
 // red #650100
