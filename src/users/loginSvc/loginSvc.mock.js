d37f0df3b49adbd0cdf9db846a58869b4a386f70
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-27 17:46:20 -0500

    loginSvc formatting

--- src/users/loginSvc/loginSvc.mock.js
+++ src/users/loginSvc/loginSvc.mock.js
@@ -2,59 +2,59 @@
 
 angular.module('flintAndSteel')
 .factory('loginSvcMock',
-	[
-		function() {
-			var mockUserAccount = {
-				id: 1,
-				username: 'MainManDarth',
-				name: 'Darth Vader',
-				likedIdeas: [ 'mock_idea' ]
-			};
+    [
+        function() {
+            var mockUserAccount = {
+                id: 1,
+                username: 'MainManDarth',
+                name: 'Darth Vader',
+                likedIdeas: [ 'mock_idea' ]
+            };
 
-			function NotImplementedException(call) {
-				this.name = 'NotImplementedException';
-				this.message = 'Method ' + call + ' has not been implemented!';
-				this.toString = function() {
-					return this.name + ': ' + this.message;
-				};
-			}
+            function NotImplementedException(call) {
+                this.name = 'NotImplementedException';
+                this.message = 'Method ' + call + ' has not been implemented!';
+                this.toString = function() {
+                    return this.name + ': ' + this.message;
+                };
+            }
 
-			var loggedIn = false;
+            var loggedIn = false;
 
-			return {
-				checkLogin: function checkLogin(account, successCb, errorCb){
-					if (account['username'] === mockUserAccount['username']) {
-						loggedIn = true;
-					}
-					else {
-						loggedIn = false;
-					}
-				},
-				addUser: function addUser(account, successCb, errorCb) {
-					throw new NotImplementedException('addUser');
-				},
-				isUserLoggedIn: function isUserLoggedIn() {
-					return loggedIn;
-				},
-				logout: function logout() {
-					loggedIn = false;
-				},
-				getProperty: function getProperty(propertyName) {
-					return mockUserAccount[propertyName];
-				},
-				likeIdea: function likeIdea(ideaId) {
-					throw new NotImplementedException('likeIdea');
-				},
-				unlikeIdea: function unlikeIdea(ideaId) {
-					throw new NotImplementedException('unlikeIdea');
-				},
-				updateAccount: function updateAccount(account, successCb, errorCb) {
-					throw new NotImplementedException('updateAccount');
-				},
-				checkValidUsername: function checkValidUsername(username, successCb, errorCb) {
-					throw new NotImplementedException('checkValidUsername');
-				}
-			};
-		}
-	]
+            return {
+                checkLogin: function checkLogin(account, successCb, errorCb){
+                    if (account['username'] === mockUserAccount['username']) {
+                        loggedIn = true;
+                    }
+                    else {
+                        loggedIn = false;
+                    }
+                },
+                addUser: function addUser(account, successCb, errorCb) {
+                    throw new NotImplementedException('addUser');
+                },
+                isUserLoggedIn: function isUserLoggedIn() {
+                    return loggedIn;
+                },
+                logout: function logout() {
+                    loggedIn = false;
+                },
+                getProperty: function getProperty(propertyName) {
+                    return mockUserAccount[propertyName];
+                },
+                likeIdea: function likeIdea(ideaId) {
+                    throw new NotImplementedException('likeIdea');
+                },
+                unlikeIdea: function unlikeIdea(ideaId) {
+                    throw new NotImplementedException('unlikeIdea');
+                },
+                updateAccount: function updateAccount(account, successCb, errorCb) {
+                    throw new NotImplementedException('updateAccount');
+                },
+                checkValidUsername: function checkValidUsername(username, successCb, errorCb) {
+                    throw new NotImplementedException('checkValidUsername');
+                }
+            };
+        }
+    ]
 );
--- src/users/loginSvc/loginSvc.spec.js
+++ src/users/loginSvc/loginSvc.spec.js
@@ -1,231 +1,231 @@
 describe('loginSvc', function() {
-	var loginSvc, $httpBackend, $rootScope, dummyUser, dummyRes;
+    var loginSvc, $httpBackend, $rootScope, dummyUser, dummyRes;
 
-	beforeEach(module('flintAndSteel'));
+    beforeEach(module('flintAndSteel'));
 
-	beforeEach(inject(function (_loginSvc_, _$httpBackend_, _$rootScope_) {
-		loginSvc = _loginSvc_;
-		$httpBackend = _$httpBackend_;
-		$rootScope = _$rootScope_;
+    beforeEach(inject(function (_loginSvc_, _$httpBackend_, _$rootScope_) {
+        loginSvc = _loginSvc_;
+        $httpBackend = _$httpBackend_;
+        $rootScope = _$rootScope_;
 
-		dummyUser = {
-			id: 'dummy_user',
-			username: 'theBestDummy',
-			password: 'password',
-			name: 'Dummy User',
-			likedIdeas: ['mock_idea', 'dummy_idea']
-		};
+        dummyUser = {
+            id: 'dummy_user',
+            username: 'theBestDummy',
+            password: 'password',
+            name: 'Dummy User',
+            likedIdeas: ['mock_idea', 'dummy_idea']
+        };
 
-		dummyRes = dummyUser;
-		dummyRes.password = undefined;
-		dummyRes.status = 'AUTH_OKAY';
-	}));
+        dummyRes = dummyUser;
+        dummyRes.password = undefined;
+        dummyRes.status = 'AUTH_OKAY';
+    }));
 
-	it('should exist', function() {
-		expect(loginSvc).toBeDefined();
-	});
+    it('should exist', function() {
+        expect(loginSvc).toBeDefined();
+    });
 
-	afterEach(function() {
-		$httpBackend.verifyNoOutstandingExpectation();
-		$httpBackend.verifyNoOutstandingRequest();
-	});
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
 
-	describe('loginSvc.checkLogin', function() {
-		var checkLoginHandler;
+    describe('loginSvc.checkLogin', function() {
+        var checkLoginHandler;
 
-		beforeEach(function() {
-			checkLoginHandler = $httpBackend.whenPOST('/login', dummyUser)
-									.respond(200, dummyRes);
-		});
+        beforeEach(function() {
+            checkLoginHandler = $httpBackend.whenPOST('/login', dummyUser)
+                                    .respond(200, dummyRes);
+        });
 
-		it('should return a good response for valid details', function() {
-			$httpBackend.expectPOST('/login', dummyUser);
+        it('should return a good response for valid details', function() {
+            $httpBackend.expectPOST('/login', dummyUser);
 
-			loginSvc.checkLogin(dummyUser, function (data) {
-				expect(data.status).toBe('AUTH_OKAY');
-			}, function (data, status, headers, config) {
+            loginSvc.checkLogin(dummyUser, function (data) {
+                expect(data.status).toBe('AUTH_OKAY');
+            }, function (data, status, headers, config) {
 
-			});
+            });
 
-			$httpBackend.flush();
-		});
-	});
+            $httpBackend.flush();
+        });
+    });
 
-	describe('loginSvc.addUser', function() {
-		var addUserHandler, uniqueIdHandler;
+    describe('loginSvc.addUser', function() {
+        var addUserHandler, uniqueIdHandler;
 
-		beforeEach(function() {
-			addUserHandler = $httpBackend.whenPOST('/signup', dummyUser)
-								.respond(201, 'Created');
-			uniqueIdHandler = $httpBackend.whenGET('/uniqueid?for=user')
-								.respond(200, 9001);
-		});
+        beforeEach(function() {
+            addUserHandler = $httpBackend.whenPOST('/signup', dummyUser)
+                                .respond(201, 'Created');
+            uniqueIdHandler = $httpBackend.whenGET('/uniqueid?for=user')
+                                .respond(200, 9001);
+        });
 
-		it('should register a new user', function() {
-			$httpBackend.expectGET('/uniqueid?for=user');
-			$httpBackend.expectPOST('/signup', dummyUser);
+        it('should register a new user', function() {
+            $httpBackend.expectGET('/uniqueid?for=user');
+            $httpBackend.expectPOST('/signup', dummyUser);
 
-			loginSvc.addUser(dummyUser, function (data) {
-				expect(data).toBe('Created');
-			}, function (data, status, headers, config) {
+            loginSvc.addUser(dummyUser, function (data) {
+                expect(data).toBe('Created');
+            }, function (data, status, headers, config) {
 
-			});
+            });
 
-			$httpBackend.flush();
-		});
-	});
+            $httpBackend.flush();
+        });
+    });
 
-	describe('loginSvc.isUserLoggedIn', function() {
+    describe('loginSvc.isUserLoggedIn', function() {
 
-		 it('should return false for no logged in user', function() {
-	 		expect(loginSvc.isUserLoggedIn()).not.toBeTruthy();
-		 });
+         it('should return false for no logged in user', function() {
+            expect(loginSvc.isUserLoggedIn()).not.toBeTruthy();
+         });
 
-		 it('should return true for a logged in user', function() {
-		 	$rootScope.account = {
-		 		status: 'AUTH_OK'
-		 	};
+         it('should return true for a logged in user', function() {
+            $rootScope.account = {
+                status: 'AUTH_OK'
+            };
 
-		 	expect(loginSvc.isUserLoggedIn()).toBeTruthy();
-		 });
+            expect(loginSvc.isUserLoggedIn()).toBeTruthy();
+         });
 
-		 it('should return false in the case of login complications', function() {
-		 	$rootScope.account = {
-		 		status: 'AUTH_ERROR'
-		 	};
+         it('should return false in the case of login complications', function() {
+            $rootScope.account = {
+                status: 'AUTH_ERROR'
+            };
 
-		 	expect(loginSvc.isUserLoggedIn()).not.toBeTruthy();
-		 });
-	});
+            expect(loginSvc.isUserLoggedIn()).not.toBeTruthy();
+         });
+    });
 
-	describe('loginSvc.logout', function() {
+    describe('loginSvc.logout', function() {
 
-		beforeEach(function() {
-			$rootScope.account = {
-				username: 'dummy',
-				name: 'Dummy Account'
-			};
-		});
+        beforeEach(function() {
+            $rootScope.account = {
+                username: 'dummy',
+                name: 'Dummy Account'
+            };
+        });
 
-		it('should log the user out', function() {
-			loginSvc.logout();
+        it('should log the user out', function() {
+            loginSvc.logout();
 
-			expect($rootScope.account).not.toBeDefined();
-		});
-	});
+            expect($rootScope.account).not.toBeDefined();
+        });
+    });
 
-	describe('loginSvc.getProperty', function() {
+    describe('loginSvc.getProperty', function() {
 
-		beforeEach(function() {
-			$rootScope.account = dummyUser;
-		});
+        beforeEach(function() {
+            $rootScope.account = dummyUser;
+        });
 
-		it('should return a value for a defined property', function() {
-			expect(loginSvc.getProperty('username')).toBe(dummyUser.username);
-		});
+        it('should return a value for a defined property', function() {
+            expect(loginSvc.getProperty('username')).toBe(dummyUser.username);
+        });
 
-		it('should return nothing for an undefined property', function() {
-			expect(loginSvc.getProperty('password')).not.toBeDefined();
-		});
-	});
+        it('should return nothing for an undefined property', function() {
+            expect(loginSvc.getProperty('password')).not.toBeDefined();
+        });
+    });
 
-	describe('loginSvc.likeIdea', function() {
-		var dummyLikedIdea;
+    describe('loginSvc.likeIdea', function() {
+        var dummyLikedIdea;
 
-		beforeEach(function() {
-			dummyLikedIdea = 'dummy_liked_idea';
-			$rootScope.account = dummyUser;
+        beforeEach(function() {
+            dummyLikedIdea = 'dummy_liked_idea';
+            $rootScope.account = dummyUser;
 
-			spyOn(loginSvc, 'updateAccount').and.callFake(function(account, successCb, errorCb) {
-				successCb('OK');
-			});
-		});
+            spyOn(loginSvc, 'updateAccount').and.callFake(function(account, successCb, errorCb) {
+                successCb('OK');
+            });
+        });
 
-		it('should add the ideaId to the array of liked ideas', function() {
-			loginSvc.likeIdea(dummyLikedIdea);
+        it('should add the ideaId to the array of liked ideas', function() {
+            loginSvc.likeIdea(dummyLikedIdea);
 
-			expect($rootScope.account.likedIdeas).toEqual(jasmine.arrayContaining([dummyLikedIdea]));
-		});
-	});
+            expect($rootScope.account.likedIdeas).toEqual(jasmine.arrayContaining([dummyLikedIdea]));
+        });
+    });
 
-	describe('loginSvc.unlikeIdea', function() {
-		var dummyLikedIdea;
+    describe('loginSvc.unlikeIdea', function() {
+        var dummyLikedIdea;
 
-		beforeEach(function() {
-			dummyLikedIdea = 'dummy_liked_idea';
-			$rootScope.account = dummyUser;
-			$rootScope.account.likedIdeas.push(dummyLikedIdea);
+        beforeEach(function() {
+            dummyLikedIdea = 'dummy_liked_idea';
+            $rootScope.account = dummyUser;
+            $rootScope.account.likedIdeas.push(dummyLikedIdea);
 
-			spyOn(loginSvc, 'updateAccount').and.callFake(function(account, successCb, errorCb) {
-				successCb('OK');
-			});
-		});
+            spyOn(loginSvc, 'updateAccount').and.callFake(function(account, successCb, errorCb) {
+                successCb('OK');
+            });
+        });
 
-		it('should add the ideaId to the array of liked ideas', function() {
-			expect($rootScope.account.likedIdeas).toEqual(jasmine.arrayContaining([dummyLikedIdea]));
+        it('should add the ideaId to the array of liked ideas', function() {
+            expect($rootScope.account.likedIdeas).toEqual(jasmine.arrayContaining([dummyLikedIdea]));
 
-			loginSvc.unlikeIdea(dummyLikedIdea);
+            loginSvc.unlikeIdea(dummyLikedIdea);
 
-			expect($rootScope.account.likedIdeas).not.toEqual(jasmine.arrayContaining([dummyLikedIdea]));
-		});
-	});
+            expect($rootScope.account.likedIdeas).not.toEqual(jasmine.arrayContaining([dummyLikedIdea]));
+        });
+    });
 
-	describe('loginSvc.updateAccount', function() {
-		var updateAccountHandler, updatedUser;
+    describe('loginSvc.updateAccount', function() {
+        var updateAccountHandler, updatedUser;
 
-		beforeEach(function() {
-			updatedUser = dummyUser;
-			updatedUser.username = 'theBestDummyV2';
-			updateAccountHandler = $httpBackend.whenPOST('/updateaccount', updatedUser)
-										.respond(200, 'OK');
-		});
+        beforeEach(function() {
+            updatedUser = dummyUser;
+            updatedUser.username = 'theBestDummyV2';
+            updateAccountHandler = $httpBackend.whenPOST('/updateaccount', updatedUser)
+                                        .respond(200, 'OK');
+        });
 
-		it('should update the user account', function() {
-			$httpBackend.expectPOST('/updateaccount', updatedUser);
+        it('should update the user account', function() {
+            $httpBackend.expectPOST('/updateaccount', updatedUser);
 
-			loginSvc.updateAccount(updatedUser, function (data) {
-				expect(data).toBe('OK');
-			}, function (data, status, headers, config) {
+            loginSvc.updateAccount(updatedUser, function (data) {
+                expect(data).toBe('OK');
+            }, function (data, status, headers, config) {
 
-			});
+            });
 
-			$httpBackend.flush();
-		});
+            $httpBackend.flush();
+        });
 
-	});
+    });
 
-	describe('loginSvc.checkValidUsername', function() {
-		var checkValidUsernameHandler;
+    describe('loginSvc.checkValidUsername', function() {
+        var checkValidUsernameHandler;
 
-		beforeEach(function() {
-			checkValidUsernameHandler = $httpBackend.whenGET('/isuniqueuser?user=dummy')
-											.respond(200, true);
-		});
+        beforeEach(function() {
+            checkValidUsernameHandler = $httpBackend.whenGET('/isuniqueuser?user=dummy')
+                                            .respond(200, true);
+        });
 
-		it('should return true for a unique user', function() {
-			$httpBackend.expectGET('/isuniqueuser?user=dummy');
+        it('should return true for a unique user', function() {
+            $httpBackend.expectGET('/isuniqueuser?user=dummy');
 
-			loginSvc.checkValidUsername('dummy', function (data) {
-				expect(data).toBe(true);
-			}, function (data, status, headers, config) {
+            loginSvc.checkValidUsername('dummy', function (data) {
+                expect(data).toBe(true);
+            }, function (data, status, headers, config) {
 
-			});
+            });
 
-			$httpBackend.flush();
-		});
+            $httpBackend.flush();
+        });
 
-		it('should return false for a duplicate user', function() {
-			checkValidUsernameHandler.respond(200, false);
-			$httpBackend.expectGET('/isuniqueuser?user=dummy');
+        it('should return false for a duplicate user', function() {
+            checkValidUsernameHandler.respond(200, false);
+            $httpBackend.expectGET('/isuniqueuser?user=dummy');
 
-			loginSvc.checkValidUsername('dummy', function (data) {
-				expect(data).toBe(false);
-			}, function (data, status, headers, config) {
+            loginSvc.checkValidUsername('dummy', function (data) {
+                expect(data).toBe(false);
+            }, function (data, status, headers, config) {
 
-			});
+            });
 
-			$httpBackend.flush();
-		});
-	});
+            $httpBackend.flush();
+        });
+    });
 
 });
