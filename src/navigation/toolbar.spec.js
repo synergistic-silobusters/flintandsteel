5e7717f3256338805cfd73cc51f585fcbc06d108
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-11-14 15:24:35 -0600

    updated tests for toast service

--- src/navigation/toolbar.spec.js
+++ src/navigation/toolbar.spec.js
@@ -9,24 +9,28 @@
 describe('ToolbarCtrl', function() {
     "use strict";
 
-    var scope, ctrl, $state, $stateParams, $mdSidenav, loginSvcMock;
+    var scope, ctrl, $state, $stateParams, $mdSidenav, loginSvcMock, $mdDialog, toastSvc;
 
     beforeEach(module('flintAndSteel'));
     beforeEach(module('ui.router'));
 
-    beforeEach(inject(function($rootScope, $controller, _$state_, _$stateParams_, _$mdSidenav_, _loginSvcMock_) {
+    beforeEach(inject(function($rootScope, $controller, _$state_, _$stateParams_, _$mdSidenav_, _loginSvcMock_, _$mdDialog_, _toastSvc_) {
         scope = $rootScope.$new();
         $state = _$state_;
         $stateParams = _$stateParams_;
         $mdSidenav = _$mdSidenav_;
         loginSvcMock = _loginSvcMock_;
+        $mdDialog = _$mdDialog_;
+        toastSvc = _toastSvc_;
 
         ctrl = $controller('ToolbarCtrl', {
             $scope: scope,
             $state: $state,
             $stateParams: $stateParams,
             $mdSidenav: $mdSidenav,
-            loginSvc: loginSvcMock
+            loginSvc: loginSvcMock,
+            $mdDialog: $mdDialog,
+            toastSvc: toastSvc
         });
     }));
 
