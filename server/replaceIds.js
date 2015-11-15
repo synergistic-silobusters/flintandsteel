505f8bcdee0273570907e46d78793e04e69c0ac7
Robert Anderson <r-anderson.9@onu.edu>
2015-11-15 13:43:23 -0500

    Fix jshint.
    Forgot a damn semicolon.

--- server/replaceIds.js
+++ server/replaceIds.js
@@ -218,7 +218,7 @@ module.exports = function(db) {
             cb(null, data);
         }, function() {
             var err = "Error replacing IDs";
-            console.log(err)
+            console.log(err);
             cb(err);
         });
     };
