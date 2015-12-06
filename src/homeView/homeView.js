f0de5324a677e4aa556f4be7791c1b91cb76d79d
Braun Brennecke <bcbrennecke@gmail.com>
2015-12-05 23:20:06 -0500

    Made major changes to homepage.  Relayout, added picture background, added ideas to homepage when searching

--- src/homeView/homeView.js
+++ src/homeView/homeView.js
@@ -3,13 +3,43 @@
 angular.module('flintAndSteel')
 .controller('HomeViewCtrl',
     [
-        '$scope', '$state',
-        function($scope, $state) {
+        '$scope', '$state','$mdSidenav', 'ideaSvc',
+        function($scope, $state, $mdSidenav, ideaSvc) {
             "use strict";
 
             $scope.navToBrowse = function navToBrowse() {
                 $state.go('ideabrowse');
             };
+
+            $scope.hoverInLeft = function(){
+                this.hoverLeft = true;
+            };
+
+            $scope.hoverOutLeft = function(){
+                 this.hoverLeft = false;
+            };
+
+            $scope.hoverInCenter = function(){
+                this.hoverCenter = true;
+            };
+
+            $scope.hoverOutCenter = function(){
+                 this.hoverCenter = false;
+            };
+
+            $scope.hoverInRight = function(){
+                this.hoverRight = true;
+            };
+
+            $scope.hoverOutRight = function(){
+                 this.hoverRight = false;
+            };
+
+            ideaSvc.getIdeaHeaders(function getIdeaHeadersSuccess(data) {
+                $scope.topIdeas = data;
+            }, function getIdeaHeadersError(data, status) {
+                console.log(status);
+            });
         }
     ]
 );
