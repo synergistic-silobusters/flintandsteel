1bff193a0b8a73ceaa57abb2e617fed76bfd2de7
Bob Anderson <rpanderson@ra.rockwell.com>
2015-12-16 16:34:45 -0500

    All Ideas now updates if a new idea is submitted
    If someone is looking at the All Ideas page and a new idea comes up, the
    ideas get reloaded.

--- src/ideas/ideaBrowseView/ideaBrowseView.js
+++ src/ideas/ideaBrowseView/ideaBrowseView.js
@@ -12,6 +12,18 @@ angular.module('flintAndSteel')
             }, function getIdeaHeadersError(data, status) {
                 console.log(status);
             });
-        }   
+
+            var ideaAddEvents = new EventSource('/ideaheaders/events');
+            ideaAddEvents.addEventListener("newHeaders", function(event) {
+                var headers = JSON.parse(event.data);
+                $scope.$apply(function() {
+                    $scope.topIdeas = headers;
+                });
+            });
+
+            $scope.$on('$stateChangeStart', function() {
+                ideaAddEvents.close();
+            });
+        }
     ]
 );
