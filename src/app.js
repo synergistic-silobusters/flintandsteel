1d74edac99e3a30ad080414df019a4f0ad608b81
ddthomps <daviddt17@gmail.com>
2015-12-28 06:42:12 -0500

    Fixed dot notation for cache fix

--- src/app.js
+++ src/app.js
@@ -108,7 +108,7 @@ angular.module('flintAndSteel', [
         // Disable IE ajax request caching
         $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
         $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
-        $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';        
+        $httpProvider.defaults.headers.get.Pragma = 'no-cache';
     }
 ]);
 
