0596e9342c78a852f6b555aab1738dafff3f5b11
Robert Anderson <r-anderson.9@onu.edu>
2016-01-15 20:49:50 -0500

    Restructure test file.
    Test cases are now grouped by the function they are testing.

--- src/ideas/addIdeaView/addIdeaView.spec.js
+++ src/ideas/addIdeaView/addIdeaView.spec.js
@@ -37,124 +37,132 @@ describe('AddIdeaViewCtrl', function() {
         expect(ctrl).toBeDefined();
     });
 
-    it('should add a new idea', function() {
-        var idea = {
-            title: 'Test Title',
-            description: 'This is a test idea.',
-            tags: ['TestTag1', 'TestTag2']
-        };
-        scope.addNewIdea(idea);
+    describe('scope.addNewIdea', function() {
 
-        expect(ideaSvcMock.postIdea).toHaveBeenCalled();
-        expect(idea.eventId).toBe("");
-        expect(idea.rolesreq.length).toBe(0);
+        it('should add a new idea', function() {
+            var idea = {
+                title: 'Test Title',
+                description: 'This is a test idea.',
+                tags: ['TestTag1', 'TestTag2']
+            };
+            scope.addNewIdea(idea);
+
+            expect(ideaSvcMock.postIdea).toHaveBeenCalled();
+            expect(idea.eventId).toBe("");
+            expect(idea.rolesreq.length).toBe(0);
+        });
+
+        it('should use the user\'s _id as the authorId', function() {
+            var idea = {
+                title: 'Test Title',
+                authorId: 3,
+                description: 'This is a test idea.',
+                tags: ['TestTag1', 'TestTag2']
+            };
+            scope.addNewIdea(idea);
+
+            expect(idea.authorId).not.toBe(3);
+            expect(idea.authorId).toBe(1);
+        });
     });
 
-    it('should use the user\'s _id as the authorId', function() {
-        var idea = {
-            title: 'Test Title',
-            authorId: 3,
-            description: 'This is a test idea.',
-            tags: ['TestTag1', 'TestTag2']
-        };
-        scope.addNewIdea(idea);
+    describe('scope.addTag', function() {
 
-        expect(idea.authorId).not.toBe(3);
-        expect(idea.authorId).toBe(1);
+        it('should add tag', function() {
+            scope.idea = {
+                title: 'Test Title',
+                authorId: 3,
+                description: 'This is a test idea.',
+                tags: ['TestTag1', 'TestTag2']
+            };
+            scope.addTag('testTag3');
+
+            expect(scope.idea.tags.length).not.toBe(2);
+            expect(scope.idea.tags.length).toBe(3);
+            scope.idea = {};
+        });
+
+        it('should not add duplicate tags', function() {
+            scope.idea = {
+                title: 'Test Title',
+                authorId: 3,
+                description: 'This is a test idea.',
+                tags: ['TestTag1', 'TestTag2']
+            };
+            scope.addTag('TestTag2');
+
+            expect(scope.idea.tags.length).not.toBe(3);
+            expect(scope.idea.tags.length).toBe(2);
+        });
+
+        it('should not add more than 5 tags', function() {
+            scope.idea = {
+                title: 'Test Title',
+                authorId: 3,
+                description: 'This is a test idea.',
+                tags: ['TestTag1', 'TestTag2', 'TestTag3', 'TestTag4', 'TestTag5']
+            };
+            scope.addTag('TestTag6');
+
+            expect(scope.idea.tags.length).not.toBe(6);
+            expect(scope.idea.tags.length).toBe(5);
+        });
+
+        it('should not add a blank tag', function() {
+            scope.idea = {
+                title: 'Test Title',
+                authorId: 3,
+                description: 'This is a test idea.',
+                tags: ['TestTag1', 'TestTag2']
+            };
+            scope.addTag('');
+
+            expect(scope.idea.tags.length).not.toBe(3);
+            expect(scope.idea.tags.length).toBe(2);
+        });
+
+        it('should remove special characters from tags', function() {
+            scope.idea = {
+                title: 'Test Title',
+                authorId: 3,
+                description: 'This is a test idea.',
+                tags: ['TestTag1', 'TestTag2']
+            };
+            scope.addTag('hello!@world&*@');
+
+            expect(scope.idea.tags.length).toBe(3);
+            expect(scope.doesTagExist('hello!@world&*@')).toBe(false);
+            expect(scope.doesTagExist('HelloWorld')).toBe(true);
+        });
+
+        it('should use CamelCase for tags', function() {
+            scope.idea = {
+                title: 'Test Title',
+                authorId: 3,
+                description: 'This is a test idea.',
+                tags: ['TestTag1', 'TestTag2']
+            };
+            scope.addTag('This is a tag');
+
+            expect(scope.idea.tags.length).toBe(3);
+            expect(scope.doesTagExist('This is a tag')).toBe(false);
+            expect(scope.doesTagExist('ThisIsATag')).toBe(true);
+        });
     });
 
-    it('should add tag', function() {
-        scope.idea = {
-            title: 'Test Title',
-            authorId: 3,
-            description: 'This is a test idea.',
-            tags: ['TestTag1', 'TestTag2']
-        };
-        scope.addTag('testTag3');
+    describe('scope.deleteTag', function() {
+        it('should delete tag', function() {
+            scope.idea = {
+                title: 'Test Title',
+                authorId: 3,
+                description: 'This is a test idea.',
+                tags: ['TestTag1', 'TestTag2']
+            };
+            scope.removeTag('TestTag2');
 
-        expect(scope.idea.tags.length).not.toBe(2);
-        expect(scope.idea.tags.length).toBe(3);
-        scope.idea = {};
-    });
-
-    it('should delete tag', function() {
-        scope.idea = {
-            title: 'Test Title',
-            authorId: 3,
-            description: 'This is a test idea.',
-            tags: ['TestTag1', 'TestTag2']
-        };
-        scope.removeTag('TestTag2');
-
-        expect(scope.idea.tags.length).not.toBe(2);
-        expect(scope.idea.tags.length).toBe(1);
-        scope.idea = {};
-    });
-
-    it('should not add duplicate tags', function() {
-        scope.idea = {
-            title: 'Test Title',
-            authorId: 3,
-            description: 'This is a test idea.',
-            tags: ['TestTag1', 'TestTag2']
-        };
-        scope.addTag('TestTag2');
-
-        expect(scope.idea.tags.length).not.toBe(3);
-        expect(scope.idea.tags.length).toBe(2);
-    });
-
-    it('should not add more than 5 tags', function() {
-        scope.idea = {
-            title: 'Test Title',
-            authorId: 3,
-            description: 'This is a test idea.',
-            tags: ['TestTag1', 'TestTag2', 'TestTag3', 'TestTag4', 'TestTag5']
-        };
-        scope.addTag('TestTag6');
-
-        expect(scope.idea.tags.length).not.toBe(6);
-        expect(scope.idea.tags.length).toBe(5);
-    });
-
-    it('should not add a blank tag', function() {
-        scope.idea = {
-            title: 'Test Title',
-            authorId: 3,
-            description: 'This is a test idea.',
-            tags: ['TestTag1', 'TestTag2']
-        };
-        scope.addTag('');
-
-        expect(scope.idea.tags.length).not.toBe(3);
-        expect(scope.idea.tags.length).toBe(2);
-    });
-
-    it('should remove special characters from tags', function() {
-        scope.idea = {
-            title: 'Test Title',
-            authorId: 3,
-            description: 'This is a test idea.',
-            tags: ['TestTag1', 'TestTag2']
-        };
-        scope.addTag('hello!@world&*@');
-
-        expect(scope.idea.tags.length).toBe(3);
-        expect(scope.doesTagExist('hello!@world&*@')).toBe(false);
-        expect(scope.doesTagExist('HelloWorld')).toBe(true);
-    });
-
-    it('should use CamelCase for tags', function() {
-        scope.idea = {
-            title: 'Test Title',
-            authorId: 3,
-            description: 'This is a test idea.',
-            tags: ['TestTag1', 'TestTag2']
-        };
-        scope.addTag('This is a tag');
-
-        expect(scope.idea.tags.length).toBe(3);
-        expect(scope.doesTagExist('This is a tag')).toBe(false);
-        expect(scope.doesTagExist('ThisIsATag')).toBe(true);
+            expect(scope.idea.tags.length).not.toBe(2);
+            expect(scope.idea.tags.length).toBe(1);
+            scope.idea = {};
+        });
     });
 });
