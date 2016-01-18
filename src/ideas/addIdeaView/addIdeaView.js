3feb3b516e2b423bbaed3cd39d247c84b3a2df17
Robert Anderson <r-anderson.9@onu.edu>
2016-01-18 00:12:04 -0500

    Use new directive for addIdeasView

--- src/ideas/addIdeaView/addIdeaView.js
+++ src/ideas/addIdeaView/addIdeaView.js
@@ -16,59 +16,6 @@ angular.module('flintAndSteel')
             $scope.idea = {};
             $scope.idea.tags = [];
             $scope.idea.eventId = "";
-            $scope.tagInput = "";
-
-            var nullEvent = {
-                _id: "",
-                name: "No Event"
-            };
-
-            ///////////////////
-            // TAG FUNCTIONS //
-            ///////////////////
-            $scope.doesTagExist = function doesTagExist(tag) {
-                if ($scope.idea.tags.indexOf(tag) === -1) {
-                    return false;
-                }
-                return true;
-            };
-
-            $scope.addTag = function addTag(tag) {
-                var reNonAlpha = /[.,-\/#!$%\^&\*;:{}=\-_`~()<>\'\"@\[\]\|\\\?]/g;
-                tag = tag.replace(reNonAlpha, " ");
-                tag = _.capitalize(_.camelCase(tag));
-                if ($scope.idea.tags.length !== 5 && !$scope.doesTagExist(tag) && tag !== '') {
-                    $scope.idea.tags.push(tag);
-                }
-            };
-
-            $scope.tagKeyEvent = function tagKeyEvent(keyEvent) {
-                // Enter
-                if (keyEvent.keyCode === 13) {
-                    $scope.addTag($scope.tagInput);
-                    $scope.tagInput = "";
-                }
-            };
-
-            $scope.removeTag = function removeTag(tag) {
-                var index = $scope.idea.tags.indexOf(tag);
-                if (index >= 0) {
-                    $scope.idea.tags.splice(index, 1);
-                }
-            };
-
-            /////////////////////
-            // EVENT FUNCTIONS //
-            /////////////////////
-
-            $scope.loadEvents = function() {
-                eventSvc.getEvents().then(function getEventsSuccess(response) {
-                    $scope.events = [nullEvent].concat(response.data);
-                }, function getEventsError(response) {
-                    $scope.events = [];
-                    console.log(response);
-                });
-            };
 
             ////////////////////
             // IDEA FUNCTIONS //
