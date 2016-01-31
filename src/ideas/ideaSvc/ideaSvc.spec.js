377ee6bbabe8cef5feb50528d603ad349941c40b
Robert Anderson <r-anderson.9@onu.edu>
2016-01-31 01:07:53 -0500

    Replaced time properties with timeCreated

--- src/ideas/ideaSvc/ideaSvc.spec.js
+++ src/ideas/ideaSvc/ideaSvc.spec.js
@@ -28,14 +28,14 @@ describe('ideaSvc', function() {
                 {
                     text: 'Yeah, mhm.',
                     name: 'Dude',
-                    time: moment().subtract(4, 'days').calendar()
+                    timeCreated: moment().subtract(4, 'days').calendar()
                 }
             ],
             backs: [
                 {
                     text: 'Some resources',
                     name: 'Some Guy',
-                    time: moment().calendar(),
+                    timeCreated: moment().calendar(),
                     types: [
                         { name: 'Experience' },
                         { name: 'Knowledge' }
@@ -264,7 +264,7 @@ describe('ideaSvc', function() {
                 _id: 'dummy_back',
                 text: 'Just Experience',
                 name: 'Some Guy',
-                time: moment().calendar(),
+                timeCreated: moment().calendar(),
                 types: [
                     { name: 'Experience' }
                 ]
