f1a4a69a87e4151024b67a140501594fcfcc5ff7
Braun Brennecke <bcbrennecke@gmail.com>
2016-02-14 17:18:54 -0500

    Refactored learn page items into a directive

--- src/learn/learn.js
+++ src/learn/learn.js
@@ -3,7 +3,30 @@
 angular.module('flintAndSteel')
 .controller('LearnCtrl',
     [
-        
 
+        '$document', '$scope',
+        function($document, $scope) {
+            "use strict";
+
+            $scope.learnItem1 = {title: "Submit an Idea",
+                                imgSource: "learn/learnToSubmitIdea.gif",
+                                description: "Ideas are the foundation of 'Innovate'.  They can consist of a suggestion to fix a business problem, a technical problem, or a general innovation for the company."
+                                };
+            $scope.learnItem2 = {title: "Search Ideas",
+                                imgSource: "learn/learnToSearch.gif",
+                                description: "Searching is how you can find an ideas that have been submitted across the company.  The search bar looks through an ideas title, content, tags, and backs."
+                                };
+            $scope.learnItem3 = {title: "Like an Idea",
+                                imgSource: "learn/learnToLike.gif",
+                                description: "Likes are the simplest form of collaboration with an idea.  This gives the author a little boost of confidence to keep going!"
+                                };
+            $scope.learnItem4 = {title: "Comment on an Idea",
+                                imgSource: "learn/learnToComment.gif",
+                                description: "Comments are a way to give feedback to the author or team of an idea.  One could give ideas or constructive criticism to further the innovation."
+                                };
+
+            $scope.LearnItems = [$scope.learnItem1, $scope.learnItem2, $scope.learnItem3, $scope.learnItem4];
+
+        }
     ]
 );
